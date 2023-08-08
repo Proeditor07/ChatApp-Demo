0dc9fd3427e4a71df4791aa5d034d0d10e21bbb3
Proeditor07 <adityathosar86@gmail.com>
2023-08-08 16:51:06 +0530

    new changes in index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
-
+app.use(cors(
+  {
+    origin:[""],
+    methods:["POST", "GET"],
+    credentials: true
+  }
+));
 
 app.use(function (req, res, next) {
   //Enabling CORS
@@ -15,13 +21,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"); 
   next();
   });
-  app.use(cors(
-    {
-      origin:[""],
-      methods:["POST", "GET"],
-      credentials: true
-    }
-  ));
+
 app.use(express.json());
 
 mongoose
