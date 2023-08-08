59a3cc94b6c933ba0d006d6452a5ef3e4b7ae327
Proeditor07 <adityathosar86@gmail.com>
2023-08-08 21:34:50 +0530

    commenting code

--- server/index.js
+++ server/index.js
@@ -6,21 +6,21 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
-app.use(cors(
-  {
-    origin:["https://chat-app-demo-frontend.vercel.app"],
-    methods:["POST", "GET"],
-    credentials: true
-  }
-));
-
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"); 
-  next();
-  });
+// app.use(cors(
+//   {
+//     origin:["https://chat-app-demo-frontend.vercel.app"],
+//     methods:["POST", "GET"],
+//     credentials: true
+//   }
+// ));
+app.use(cors());
+// app.use(function (req, res, next) {
+//   //Enabling CORS
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"); 
+//   next();
+//   });
 
 app.use(express.json());
 
