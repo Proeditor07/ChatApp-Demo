b57672e6f5f2976a0608b9ffc55ef53533c5716f
Proeditor07 <adityathosar86@gmail.com>
2023-08-08 16:45:45 +0530

    new changes in index.js

--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,3 @@
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"); 
-  next();
-  });
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,11 +1,4 @@
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"); 
-    next();
-    });
-export const host = "https://chat-app-demo-one.vercel.app";
+export const host = "https://chat-app-demo-frontend.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`; 
